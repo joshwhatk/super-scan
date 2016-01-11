0d27296da41cfd556a4b80d2d0d3eb376d774196
Josh Friend <joshwhatk@gmail.com>
2016-01-11 17:03:21 -0500

    added getExcludedDirectories... and periods

--- src/Contracts/AccountInterface.php
+++ src/Contracts/AccountInterface.php
@@ -15,26 +15,32 @@ namespace Joshwhatk\SuperScan\Contracts;
 interface AccountInterface
 {
     /**
-     * Get the name of the Server for the Account
+     * Get the name of the Server for the Account.
      * @return string
      */
     public function getServerName();
 
     /**
-     * Get the IP Address of the Server for the Account
+     * Get the IP Address of the Server for the Account.
      * @return string
      */
     public function getIpAddress();
 
     /**
-     * Get the Webroot of the Website for the Account
+     * Get the Webroot of the Website for the Account.
      * @return string
      */
     public function getWebroot();
 
     /**
-     * Get the URL of the Website for the Account
+     * Get the URL of the Website for the Account.
      * @return string
      */
     public function getUrl();
+
+    /**
+     * Get a Collection of excluded file paths.
+     * @return \Illuminate\Support\Collection
+     */
+    public function getExcludedDirectories();
 }
