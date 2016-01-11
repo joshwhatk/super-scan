8ac44336f1d96a008838073fc51ed208ab301941
Josh Friend <joshwhatk@gmail.com>
2016-01-11 13:47:56 -0500

    added AccountInterface to require the availability of account variables

---
+++ src/Contracts/AccountInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Part of the SuperScan package.
+ *
+ * @package    SuperScan
+ * @version    0.0.1
+ * @author     joshwhatk
+ * @license    MIT
+ * @link       http://jwk.me
+ */
+
+namespace Joshwhatk\SuperScan\Contracts;
+
+interface AccountInterface
+{
+    /**
+     * Get the name of the Server for the Account
+     * @return string
+     */
+    public function getServerName();
+
+    /**
+     * Get the IP Address of the Server for the Account
+     * @return string
+     */
+    public function getIpAddress();
+
+    /**
+     * Get the Webroot of the Website for the Account
+     * @return string
+     */
+    public function getWebroot();
+
+    /**
+     * Get the URL of the Website for the Account
+     * @return string
+     */
+    public function getUrl();
+}
