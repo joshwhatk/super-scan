570660ccb3e5d88dcedabf45ce14b197c0797ce9
Josh Friend <joshwhatk@gmail.com>
2016-01-11 17:02:39 -0500

    setup default configuration

--- config/joshwhatk.super_scan.php
+++ config/joshwhatk.super_scan.php
@@ -19,4 +19,57 @@ return [
         ],
     ],
 
+    'defaults' => [
+
+        'extensions' => [
+
+            /*
+            |--------------------------------------------------------------------------
+            | Extensions to Check
+            |--------------------------------------------------------------------------
+            |
+            | Examples: ['php', 'html', 'htm', 'js']
+            |
+            | Recommended: An empty array will return ALL extensions which is best
+            | for real security.
+            |
+            */
+
+            'whitelist' => [],
+
+            /*
+            |--------------------------------------------------------------------------
+            | Extensions to Exclude from the Check
+            |--------------------------------------------------------------------------
+            |
+            | Examples: ['ftpquota', 'txt', 'swf', 'fla']
+            |
+            | This will only be used if the 'whitelist' config is empty. If both are
+            | empty arrays all file extensions will be checked.
+            |
+            */
+
+            'blacklist' => ['ftpquota', 'txt', 'swf', 'fla'],
+
+            /*
+            |--------------------------------------------------------------------------
+            | Scan Extension-less files?
+            |--------------------------------------------------------------------------
+            */
+
+            'scan_extensionless' => false,
+        ],
+
+        /*
+        |--------------------------------------------------------------------------
+        | Directories to ignore
+        |--------------------------------------------------------------------------
+        */
+
+        'directories' => [
+
+            'blacklist' => ['protected', 'private', '.git'],
+        ],
+    ],
+
 ];
