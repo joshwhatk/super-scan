99af5dae9f81333b15fee63db69f9e48e8c02b50
Josh Friend <joshwhatk@gmail.com>
2016-01-11 12:21:13 -0500

    added header to Scan

--- src/Scan.php
+++ src/Scan.php
@@ -2,6 +2,16 @@
 
 namespace Joshwhatk\SuperScan;
 
+/**
+ * Part of the Superscan package.
+ *
+ * @package    Superscan
+ * @version    0.0.1
+ * @author     joshwhatk
+ * @license    MIT
+ * @link       http://jwk.me
+ */
+
 class Scan
 {
     //
