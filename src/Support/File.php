f5888fcc15d29538a6fe77435ac2e6060e08e45a
Joshua Friend <joshwhatk@gmail.com>
2016-01-11 23:14:23 -0500

    add File class to hold File attributes

---
+++ src/Support/File.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Joshwhatk\SuperScan\Support;
+
+use Carbon\Carbon;
+use Joshwhatk\SuperScan\Database\BaselineFile;
+
+class File
+{
+    public $path;
+    public $hash;
+    public $last_modified;
+
+    public function __construct($file)
+    {
+        if(is_a($file, BaselineFile::class))
+        {
+            $this->path = $file->path;
+            $this->hash = $file->hash;
+            $this->last_modified = $file->last_modified;
+
+            return $this;
+        }
+
+        $this->path = $file;
+        $this->hash = hash_file("sha1", $file_path);
+        $this->last_modified = new Carbon(filemtime($file_path));
+
+        return $this;
+    }
+
+    public function toArray($account = null)
+    {
+        $array = [
+            'path' => $this->path,
+            'hash' => $this->hash,
+            'last_modified' => $this->last_modified,
+        ];
+
+        if(! is_null($account))
+        {
+            $array['account_id'] = $account->id;
+        }
+
+        return $array;
+    }
+}
