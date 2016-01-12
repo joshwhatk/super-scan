dfe0ad539c19c364b0390b3c12adfd1b9001ab75
Joshua Friend <joshwhatk@gmail.com>
2016-01-12 17:44:30 -0500

    version changes

--- src/Scan.php
+++ src/Scan.php
@@ -6,7 +6,7 @@ namespace Joshwhatk\SuperScan;
  * Part of the SuperScan package.
  *
  * @package    SuperScan
- * @version    0.0.1
+ * @version    0.0.2
  * @author     joshwhatk
  * @license    MIT
  * @link       http://jwk.me
