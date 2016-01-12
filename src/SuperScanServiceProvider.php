264714e3545694aaf1f9ab7cb4693513de62888c
Joshua Friend <joshwhatk@gmail.com>
2016-01-12 18:49:43 -0500

    renamed file

---
+++ src/SuperScanServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Joshwhatk\SuperScan;
+
+/**
+ * Part of the SuperScan package.
+ *
+ * @package    SuperScan
+ * @version    0.0.4
+ * @author     joshwhatk
+ * @license    MIT
+ * @link       http://jwk.me
+ */
+
+use Illuminate\Support\ServiceProvider;
+
+class SuperScanServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/joshwhatk.super_scan.php' => config_path('joshwhatk.super_scan.php'),
+            __DIR__.'/../database/migrations/' => database_path('migrations')
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
