2354915ecf81ff29794f846e17fe7398a90e29b2
Joshua Friend <joshwhatk@gmail.com>
2016-03-29 22:50:51 -0400

    setting up ReportingInterface

--- src/Contracts/ReportingInterface.php
+++ src/Contracts/ReportingInterface.php
@@ -12,7 +12,36 @@
 
 namespace JoshWhatK\SuperScan\Contracts;
 
+use JoshWhatK\SuperScan\Scan;
+
 interface ReportingInterface
 {
-    //
+    /**
+     * The Scan is added to the report after
+     * it is completed, this is the method
+     * to accomplish that.
+     *
+     * @param Scan $scan
+     * @return void
+     */
+    public function addScan(Scan $scan);
+
+    /**
+     * The main reporting method.
+     * This should build up the report
+     * and send out any emails.
+     *
+     * @return void
+     */
+    public function report();
+
+    /**
+     * Add an extra message to the report.
+     * This is mostly used if the report does
+     * not run as expected.
+     *
+     * @param  string $message
+     * @return void
+     */
+    public function alert($message);
 }
